927cf0b575b27323534ab214c9bc8f113b6db2e8
cloudgyb <gyb_cloud@163.com>
2023-02-19 21:14:44 +0800

    feat: WebsocketServer

---
+++ src/main/java/com/github/cloudgyb/websocketserver/TextFrameHandler.java
@@ -0,0 +1,21 @@
+package com.github.cloudgyb.websocketserver;
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author geng
+ * @since 2023/02/19 19:39:27
+ */
+@ChannelHandler.Sharable
+public class TextFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, TextWebSocketFrame frame) {
+        String s = frame.content().toString(StandardCharsets.UTF_8);
+        System.out.println(s);
+    }
+}
---
+++ src/main/java/com/github/cloudgyb/websocketserver/WebsocketServer.java
@@ -0,0 +1,58 @@
+package com.github.cloudgyb.websocketserver;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author geng
+ * @since 2023/2/13 15:48
+ */
+public class WebsocketServer {
+    private final static Logger logger = LoggerFactory.getLogger(com.github.cloudgyb.netty.EchoServer.class);
+
+    public static void main(String[] args) {
+        int port = 9090;
+        NioEventLoopGroup boss = new NioEventLoopGroup();
+        NioEventLoopGroup worker = new NioEventLoopGroup();
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+        TextFrameHandler textFrameHandler = new TextFrameHandler();
+        try {
+            ChannelFuture channelFuture = serverBootstrap
+                    .group(boss, worker)
+                    .channel(NioServerSocketChannel.class)
+                    .option(ChannelOption.SO_BACKLOG, 10)
+                    .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                        @Override
+                        protected void initChannel(NioSocketChannel socketChannel) {
+                            socketChannel.pipeline()
+                                    .addLast(new LoggingHandler(LogLevel.DEBUG))
+                                    .addLast(new HttpRequestDecoder())
+                                    .addLast(new HttpResponseEncoder())
+                                    .addLast(new HttpObjectAggregator(2048, true))
+                                    .addLast(new WebSocketServerProtocolHandler("/chat", "chat",
+                                            true, 2028, 2000))
+                                    .addLast(textFrameHandler);
+                        }
+                    })
+                    .bind(port);
+            channelFuture.addListener(future -> logger.info("Server listen at " + port + "...."));
+            channelFuture.channel().closeFuture().sync(); //同步等待通道关闭，避免程序退出。
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            worker.shutdownGracefully();
+            boss.shutdownGracefully();
+        }
+    }
+}
